2034abc15a9e85fc88a51dab4b627334f0e40e7c
Maria <mariacristinaiosub@protonmail.com>
2022-08-07 22:02:30 +0300

    Update
    Added variables and rearranged the code.

--- script.js
+++ script.js
@@ -1,21 +1,30 @@
+const cat = {
+  "name": "Luna",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["Bruno", "Liz"],
+  "greeting": "Meow :3"
+};
+console.log(cat.greeting);
+
+const today = new Date();
+let currentYear = today.getFullYear();
+document.getElementById("year").innerHTML = currentYear;
+
 /*** BULMA JS ***/
 document.addEventListener('DOMContentLoaded', () => {
-    // Functions to open and close a modal
     function openModal($el) {
       $el.classList.add('is-active');
     }
-  
     function closeModal($el) {
       $el.classList.remove('is-active');
     }
-  
     function closeAllModals() {
       (document.querySelectorAll('.modal') || []).forEach(($modal) => {
         closeModal($modal);
       });
     }
-  
-    // Add a click event on buttons to open a specific modal
+
     (document.querySelectorAll('.js-modal-trigger') || []).forEach(($trigger) => {
       const modal = $trigger.dataset.target;
       const $target = document.getElementById(modal);
@@ -26,7 +35,6 @@ document.addEventListener('DOMContentLoaded', () => {
       });
     });
   
-    // Add a click event on various child elements to close the parent modal
     (document.querySelectorAll('.modal-background, .modal-close, .modal-card-head .delete, .modal-card-foot .button') || []).forEach(($close) => {
       const $target = $close.closest('.modal');
   
@@ -35,7 +43,6 @@ document.addEventListener('DOMContentLoaded', () => {
       });
     });
   
-    // Add a keyboard event to close all modals
     document.addEventListener('keydown', (event) => {
       const e = event || window.event;
   
